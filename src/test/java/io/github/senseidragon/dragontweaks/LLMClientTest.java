5c5cf7f5015a1cb539e2c6f32570f68357b61581
Senseidragon <senseidragon@gmail.com>
2026-04-22 14:35:26 -0400

    fix: rename OllamaClientTest to LLMClientTest, fix stale Ollama comment in Config

--- src/test/java/io/github/senseidragon/dragontweaks/LLMClientTest.java
+++ src/test/java/io/github/senseidragon/dragontweaks/LLMClientTest.java
@@ -3,7 +3,7 @@ package io.github.senseidragon.dragontweaks;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class OllamaClientTest {
+class LLMClientTest {
 
     @Test
     void timeOfDay_morning_boundaries() {
